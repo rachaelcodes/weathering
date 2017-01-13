fa4f2c54e760d26e42809cb64d3bc44319d6ba91
rachaelcodes <rachaelcodes@users.noreply.github.com>
2017-01-13 22:24:47 +0000

    basic functionality

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-//set the location div to a variable CHECK WHICH WAY ROUND INNER HTML GOES 
-var location = document.getElementById("location").innerHTML;
+function weather() {
+//set the location div to a variable 
+//var location = document.getElementById("location");
 //set the API key and the url(https://api.forecast.io/forecast/) equal to variables.
 var key = "33a63504ed3994c34d5cdffcad78483b";
 var url = "https://api.forecast.io/forecast/";
 //write the geolocation.getCurrentPosition method with the parameters (success, error)
-var getCurrentPosition = {
-	success() {
-	var latitute = position.coords.latitude;
-	var longitude = position.coords.longitude;
-	 document.getElementById("location").innerHTML = "Latitude: " + latitude \n "Longitude: " + longitude;
-	},
-	error() {
-	
+navigator.geolocation.getCurrentPosition(success, error);
+function success(position){
+	//var latitute = position.coords.latitude;
+	//var longitude = position.coords.longitude;
+	 document.getElementById("location").innerHTML = "latitude: " + position.coords.latitude + " longitude: " + position.coords.longitude;
+	$.getJSON(url + key + "/" + position.coords.latitude + "," + position.coords.longitude + "?callback=?", function(data){
+		$('#temp').html(((data.currently.temperature - 32)*5/9).toFixed(0) + 'C');
+		$('#minutely').html(data.minutely.summary);
+	});
 	}
-};
+	
+	function error() {
+	document.getElementById("location").innerHTML = "Unfortunately, we couldn't retrieve your location.";
+	}
+	//document.getElementById("location").innerHTML = "Locating...";
+	}
+weather();
+
 //write the success function consisting of the following:
 //set the position.coords.latitude and position.coords.longitude to their own variables
 //use the location div’s variable to display the latitude and longitude for the location
