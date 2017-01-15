35d1643d5bd3b22e15b3962f83948806bdfb3c7e
rachaelcodes <rachaelcodes@users.noreply.github.com>
2017-01-15 22:27:22 +0000

    Updated with styling

--- app.js
+++ app.js
@@ -9,10 +9,42 @@ navigator.geolocation.getCurrentPosition(success, error);
 function success(position){
 	//var latitute = position.coords.latitude;
 	//var longitude = position.coords.longitude;
-	 document.getElementById("location").innerHTML = "latitude: " + position.coords.latitude + " longitude: " + position.coords.longitude;
+	 document.getElementById("location").innerHTML = "latitude: " + position.coords.latitude.toFixed(1) + "° <br />  longitude: " + position.coords.longitude.toFixed(1)+"°";
 	$.getJSON(url + key + "/" + position.coords.latitude + "," + position.coords.longitude + "?callback=?", function(data){
-		$('#temp').html(((data.currently.temperature - 32)*5/9).toFixed(0) + 'C');
+		$('#temp').html(((data.currently.temperature - 32)*5/9).toFixed(0) + '°C');
 		$('#minutely').html(data.minutely.summary);
+		switch (data.currently.icon){
+			case "cloudy":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/224755')";
+			break;
+			case "partly-cloudy-day":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/411046')";
+			break;
+			case "partly-cloudy-night":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/470178')";
+			break;
+			case "clear-day":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/472766')";
+			break;
+			case "clear-night":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/470191')";
+			break;
+			case "rain":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/403069')";
+			break;
+			case "snow":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/426534')";
+			break;
+			case "sleet":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/349213')";
+			break;
+			case "wind":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/195950')";
+			break;
+			case "fog":
+			document.body.style.backgroundImage = "url('https://source.unsplash.com/collection/1073')";
+			break;
+		}
 	});
 	}
 	
